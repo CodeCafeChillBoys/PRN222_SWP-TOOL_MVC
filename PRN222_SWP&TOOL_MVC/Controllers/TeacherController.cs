1fc9b647492d4c83270e1087acda5d3137c373f6
VoVanCuong <cuongcon2k04@gmail.com>
2026-01-24 11:12:51 +0700

    [Add]Desgin_TeacherPage

---
+++ PRN222_SWP&TOOL_MVC/Controllers/TeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN222_MVC.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
