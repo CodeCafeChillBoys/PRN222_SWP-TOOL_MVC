aceb9a100570eb6718f6564e1a0e507e77023ce7
Tran Tan Phat <trantanphat2004lp@gmail.com>
2026-02-23 21:14:17 +0700

    add entities StudentGroup

--- PRN222_SWP&TOOL_MVC.Repository/Entities/StudentGroup.cs
+++ PRN222_SWP&TOOL_MVC.Repository/Entities/StudentGroup.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PRN222_SWP_TOOL_MVC.Repository.Entities
 {
     public class StudentGroup
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int GroupID { get; set; }
         public string GroupName { get; set; }
         public int ClassID { get; set; }
